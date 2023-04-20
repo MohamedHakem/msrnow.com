1231e582f7ea26c49da8248054630fc321197da0
Mohamed Hakem <mohamedhakem959628@gmail.com>
2023-04-20 15:17:04 +0200

    Make short_slug unique, repeat if duplicate

--- utils/newSave/addArticlesToDB.js
+++ utils/newSave/addArticlesToDB.js
@@ -3,12 +3,9 @@ import generateShortSlug from "@/utils/generateShortSlug"
 import prisma from "@/lib/prisma"
 
 // add the shortSlug to all newArticles array here, and save with prisma,
-// if error, add new shortslugs and repeat until it goes through
+// if error, add new short_slug(s) and repeat until it goes through
 
 async function addArticlesToDB(newArticles, category) {
-  // const addShortSlug = (arr, key) =>
-  //   arr.map((obj) => ({ ...obj, [key]: generateShortSlug(1) }))
-
   const addShortSlug = async (arr, key) =>
     arr.map((obj) => {
       obj[key] = generateShortSlug(1)[0]
@@ -19,7 +16,6 @@ async function addArticlesToDB(newArticles, category) {
 
   addShortSlug(newArticles, "short_slug")
   let sluggedArticles = newArticles
-  // console.log("newArticles[0]: ", newArticles[0])
 
   while (true) {
     counter++
@@ -37,7 +33,6 @@ async function addArticlesToDB(newArticles, category) {
         console.log("full error: ", error)
         sluggedArticles = addShortSlug(newArticles, "short_slug")
       } else {
-        // console.log("counter: ", counter)
         console.log(
           "Error while saving article(s) to db: \n",
           "prisma error code: ",
@@ -50,43 +45,9 @@ async function addArticlesToDB(newArticles, category) {
     }
   }
 
-  // console.log("counter: ", counter)
-  // console.log("savedArticles: ", savedArticles)
-
   console.log(
     `savedArticles: ${savedArticles}, added ${savedArticles.count} articles to DB, out of ${newArticles.length} scraped articles - under ${category} category`
   )
 }
 
 export default addArticlesToDB
-
-// async function addArticlesToDB(newArticles, category) {
-//   const addShortSlug = (arr, key) =>
-//     arr.map((obj) => ({ ...obj, [key]: generateShortSlug(1) }))
-
-//   const sluggedArticles = addShortSlug(newArticles, "short_slug")
-
-//   try {
-//     const savedArticles = await prisma.article.createMany({
-//       data: sluggedArticles,
-//       skipDuplicates: true,
-//     })
-
-//     console.log("savedArticles: ", savedArticles)
-
-//     console.log(
-//       `added ${savedArticles.count} articles to DB, out of ${newArticles.length} scraped articles - under ${category} category`
-//     )
-//   } catch (error) {
-//     console.log(
-//       "Error while saving article(s) to db: \n",
-//       "prisma error code: ",
-//       error.code,
-//       "\n",
-//       error.code === "P2002"
-//         ? "some of the articles already exist, use skipDuplicates to ignore any duplicates"
-//         : error,
-//       " \n full error: \n canceled for now...development"
-//     )
-//   }
-// }
