81ce7674723c5914e96febdcc7b9e7fb55e4feba
Mohamed Hakem <mohamedhakem959628@gmail.com>
2023-04-22 21:32:58 +0200

    Change gtag script to afterinteractive to work properly

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -110,7 +110,7 @@ export default function App({ Component, pageProps }) {
 
       {/* Global Site Tag (gtag.js) - Google Analytics */}
       <Script
-        strategy="worker"
+        strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
       />
 
