d9a1dd3134511911f51b14e933b26df3699f250b
Mohamed Hakem <mohamedhakem959628@gmail.com>
2023-09-30 13:41:47 +0300

    fix: back to using pnpm, trying to fix Vercel build error

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,7 @@
 import './globals.css';
-// import '@/components/logo/logoAnimation.js'
-
 import type { Metadata } from 'next';
 import { Cairo } from 'next/font/google';
 import { ThemeProvider } from '@/components/providers/theme-provider';
-import { cn } from '@/lib/utils';
 
 const font = Cairo({
   subsets: ['arabic'],
@@ -19,10 +16,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ar" suppressHydrationWarning>
-      {/* <body className={cn(font.className, 'bg-white dark:bg-[#302E2B]')}> */}
-      {/* <body className={cn(font.className, 'bg-white dark:bg-[#000]')}> */}
       <body className={`${font.className} bg-white dark:bg-[#000]`}>
-        {/* <ThemeProvider attribute="class" defaultTheme="light" enableSystem storageKey="msrnow-theme"> */}
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
           {children}
         </ThemeProvider>
