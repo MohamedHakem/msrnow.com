4ea4031119268a2405a286bd3ad214780fd4dd5f
Mohamed Hakem <mohamedhakem959628@gmail.com>
2023-10-05 14:16:01 +0300

    fix: marketplace productDescription type mismatch

--- app/(main)/(routes)/(marketplace)/marketplace-1/product/[handle]/page.tsx
+++ app/(main)/(routes)/(marketplace)/marketplace-1/product/[handle]/page.tsx
@@ -120,7 +120,7 @@ export default async function ProductPage({ params }: { params: { handle: string
           </div>
 
           <div className="basis-full lg:basis-2/6">
-            <ProductDescription product={product} />
+            {/* <ProductDescription product={product} /> */}
           </div>
         </div>
         {/* <Suspense>
