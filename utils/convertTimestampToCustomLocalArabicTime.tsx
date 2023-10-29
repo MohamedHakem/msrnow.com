d8ce7aed3f4437a0fcb013bf924ebd7f0defbfd7
MohamedHakem <mohamedhakem959628@gmail.com>
2023-10-29 06:49:17 +0200

    fix: fix date to visitor's locale timezone

--- utils/convertTimestampToCustomLocalArabicTime.tsx
+++ utils/convertTimestampToCustomLocalArabicTime.tsx
@@ -40,45 +40,49 @@ function getLocalArabicFromTimestamp(published_at: number | Date, weekday: boole
         minutesDifference === 1
           ? 'قبل دقيقة'
           : minutesDifference === 2
-          ? 'قبل دقيقتين'
-          : minutesDifference > 2 && minutesDifference < 11
-          ? `قبل ${minutesDifference} دقائق`
-          : `قبل ${minutesDifference} دقيقة`;
+            ? 'قبل دقيقتين'
+            : minutesDifference > 2 && minutesDifference < 11
+              ? `قبل ${minutesDifference} دقائق`
+              : `قبل ${minutesDifference} دقيقة`;
     } else if (hoursDifference < 24) {
       timeAgo =
         hoursDifference === 1
           ? 'قبل ساعة'
           : hoursDifference === 2
-          ? 'قبل ساعتين'
-          : hoursDifference > 2 && hoursDifference < 11
-          ? `قبل ${hoursDifference} ساعات`
-          : `قبل ${hoursDifference} ساعة`;
+            ? 'قبل ساعتين'
+            : hoursDifference > 2 && hoursDifference < 11
+              ? `قبل ${hoursDifference} ساعات`
+              : `قبل ${hoursDifference} ساعة`;
     } else if (daysDifference < 7) {
       timeAgo =
         daysDifference === 1
           ? 'قبل يوم'
           : daysDifference === 2
-          ? 'قبل يومين'
-          : daysDifference > 2 && daysDifference < 11
-          ? `قبل ${daysDifference} أيام`
-          : `قبل ${daysDifference} يوم`;
+            ? 'قبل يومين'
+            : daysDifference > 2 && daysDifference < 11
+              ? `قبل ${daysDifference} أيام`
+              : `قبل ${daysDifference} يوم`;
     } else {
       timeAgo =
         weeksDifference === 1
           ? 'قبل اسبوع'
           : weeksDifference === 2
-          ? 'قبل اسبوعين'
-          : weeksDifference > 2 && weeksDifference < 11
-          ? `قبل ${weeksDifference} أسابيع`
-          : `قبل ${weeksDifference} اسبوع`;
+            ? 'قبل اسبوعين'
+            : weeksDifference > 2 && weeksDifference < 11
+              ? `قبل ${weeksDifference} أسابيع`
+              : `قبل ${weeksDifference} اسبوع`;
     }
 
     return timeAgo;
   }
 
+  // const currentTimeWithWeekday = weekday
+  //   ? new Date(published_at).toLocaleString('ar', weekdayOptions)
+  // : new Date(published_at).toLocaleString('ar', options);
   const currentTimeWithWeekday = weekday
-    ? new Date(published_at).toLocaleString('ar', weekdayOptions)
-    : new Date(published_at).toLocaleString('ar', options);
+    ? new Date(published_at).toLocaleString('ar-EG', weekdayOptions)
+    : new Date(published_at).toLocaleString('ar-EG', options);
+
   return currentTimeWithWeekday.replace(/،/g, ' · ');
 }
 
