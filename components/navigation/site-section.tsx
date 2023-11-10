d54a5da57dc283d4fecf13a2f9751deb0903bb63
MohamedHakem <mohamedhakem959628@gmail.com>
2023-11-10 23:10:08 +0200

    fix: handle site name for marketplace vs news sections

--- components/navigation/site-section.tsx
+++ components/navigation/site-section.tsx
@@ -5,7 +5,7 @@ import { usePathname } from 'next/navigation';
 export default function HeaderSiteSectionName() {
   const pathname = usePathname();
 
-  return <p className="w-fit h-fit text-3xl font-bold pl-2 animate-fadeIn">
-    {pathname.startsWith('/marketplace') || pathname.startsWith('/') ? "المتجر" : "الأخبار"}
+return <p className="w-fit h-fit text-3xl font-bold pl-2 animate-fadeIn">
+    {pathname.startsWith('/marketplace') || pathname === '/' ? "المتجر" : "الأخبار"}
   </p>
 }
