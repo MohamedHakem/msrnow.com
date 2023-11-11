b04fe9b8deae157816a3a42a7019f91ba5adc424
MohamedHakem <mohamedhakem959628@gmail.com>
2023-11-11 17:31:03 +0200

    style: modernize the marketplace product list

--- components/marketplace/layout/product-grid-items.tsx
+++ components/marketplace/layout/product-grid-items.tsx
@@ -1,29 +1,39 @@
-import Grid from '@/components/marketplace/grid';
-import { GridTileImage } from '@/components/marketplace/grid/tile';
-// import { Product } from '@/lib/marketplace/types';
+import Image from 'next/image';
 import Link from 'next/link';
+import Grid from '@/components/marketplace/grid';
+import Label from '@/components/marketplace/label';
+
 
 export default function ProductGridItems({ products }: { products: any[] }) {
   return (
     <>
       {products.map((product, i) => (
         <Grid.Item key={product.id} className="animate-fadeIn">
-          <Link className="relative inline-block h-full w-full" href={`/marketplace/product/${product.slug}`}>
-            <GridTileImage
-              alt={product.title}
-              label={{
-                title: product.title,
-                amount: product.price,
-                currencyCode: "EGP"
-              }}
-              src={`https://imagecdn.app/v2/image/${product.images[0]?.url}?width=490&height=490`}
-              fill
-              priority={i < 3 ? true : false}
+          <Link className="h-full w-full" href={`/marketplace/product/${product.slug}`}>
+            <Image
+              unoptimized width={490} height={490} alt={product.title} priority={i < 3 ? true : false}
+              src={`https://wsrv.nl/?url=${product.images[0]?.url}&default=${product.images[0]?.url}&l=9&af=''&il=''&n=-1&w=490&h=460&output=webp`}
               sizes="(min-width: 768px) 33vw, (min-width: 640px) 50vw, 100vw"
+              className="relative h-full w-full object-contain rounded-xl"
             />
+            <Label title={product.title} amount={product.price} currencyCode={"EGP"} />
           </Link>
         </Grid.Item>
       ))}
     </>
   );
 }
+
+
+{/* <GridTileImage
+  alt={product.title}
+  label={{
+    title: product.title,
+    amount: product.price,
+    currencyCode: "EGP"
+  }}
+  src={`https://imagecdn.app/v2/image/${product.images[0]?.url}?width=490&height=490`}
+  fill
+  priority={i < 3 ? true : false}
+  sizes="(min-width: 768px) 33vw, (min-width: 640px) 50vw, 100vw"
+/> */}
