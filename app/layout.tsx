dd603c5c17b25c2ef2c259313f4613718fbc394d
MohamedHakem <mohamedhakem959628@gmail.com>
2023-11-12 18:05:50 +0200

    fix: fix sticky header and footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                 <Header />
                 {/* <main className="grid grid-cols-[1fr] w-full relative overflow-x-hidden overflow-y-auto h-[calc(100vh-88px)] mt-[56px]"> */}
                 {/* <main className="grid grid-cols-[1fr] w-full relative overflow-x-hidden overflow-y-auto h-screen mt-[64px]"> */}
-                <main className="grid grid-cols-[1fr] w-full relative overflow-x-hidden overflow-y-auto h-[calc(100vh-96px)] mt-[64px]">
+                {/* <main className="grid grid-cols-[1fr] w-full relative overflow-x-hidden overflow-y-auto h-[calc(100vh-96px)] mt-[64px]"> */}
+                <main className="grid grid-cols-[1fr] w-full relative overflow-x-hidden overflow-y-auto h-[calc(100dvh-96px)] mt-[120px]">
                   <section className="flex flex-row flex-auto">
                     <div className="flex flex-col flex-auto gap-2 m-auto h-full animate-fadeIn">
                       {children}
