f51e5bd8b8cb25686707fd13e952a73c2f5815d6
MohamedHakem <mohamedhakem959628@gmail.com>
2023-11-12 21:04:00 +0200

    style: bottom banner thanking visitors for using msrnow.com

--- components/navigation/bottom-navbar.tsx
+++ components/navigation/bottom-navbar.tsx
@@ -4,10 +4,12 @@ export const BottomNavbar = () => {
     // <div className="flex flex-row justify-between gap-2 items-center border-t p-1 h-8">
     // <div className="absolute bottom-0 bg-white z-50 w-full flex flex-row justify-between gap-2 items-center border-t p-1 h-8">
     // <div className="bg-white z-50 w-full flex flex-row justify-between gap-2 items-center border-t p-1 h-8">
-    <div className="absolute bottom-0 bg-white z-50 w-full flex flex-row justify-between gap-2 items-center border-t p-1 h-8">
-      {arr.map((a, i) => (
+    // <div className="absolute bottom-0 bg-white z-50 w-full border-t p-1 h-8 text-center">
+    <div className="absolute bottom-0 flex bg-white z-50 w-full border-t p-1 h-10 text-center justify-center items-center">
+      <p>شكرا لإستخدامك موقع مصر-الان Msrnow.com</p>
+      {/* {arr.map((a, i) => (
         <p key={i} className="w-full h-full bg-gray-100 dark:bg-stone-700"></p>
-      ))}
+      ))} */}
     </div>
   );
 };
