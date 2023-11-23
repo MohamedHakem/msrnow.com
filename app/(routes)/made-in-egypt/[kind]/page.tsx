05147509a62de012e6b399c2e3b89294632ed3b5
MohamedHakem <mohamedhakem959628@gmail.com>
2023-11-23 21:49:14 +0200

    feat: add water to made-in-egypt catalog

--- app/(routes)/made-in-egypt/[kind]/page.tsx
+++ app/(routes)/made-in-egypt/[kind]/page.tsx
@@ -1,24 +1,57 @@
+import { productsMadeInEgypt } from "@/data/static/products-made-in-egypt"
+import Image from "next/image";
+import { notFound } from "next/navigation"
+
 export default async function madeInEgyptPage({ params }: { params: { kind: string } }) {
-  const kinds = [
-    'بدائل المياة',
-    'بدائل النسكافية',
-    'بدائل المنظفات',
-    'بدائل منتجات التجميل',
-    'بدائل جبن',
-    'بدائل اللبن المعلب',
-  ]
 
   const kind = decodeURIComponent(params.kind)
+  const EgProduct = productsMadeInEgypt.find(p => p.url === kind)
+  if (!EgProduct) return notFound();
 
   return (
-    <div className="w-full laptop:max-w-7xl py-4 flex justify-center items-center m-auto">
-      <div className="w-full flex justify-center">
-        <h1 className="text-3xl laptop:text-5xl font-bold">
-          {kind}
-        </h1>
+    <div className="w-full laptop:max-w-screen-large py-4 flex justify-center items-center m-auto">
+      <div className="w-full flex flex-col justify-center gap-4">
+        <h1 className="text-4xl laptop:text-6xl font-bold text-center">{EgProduct.title}</h1>
+        <div className="flex flex-col gap-8 p-2 justify-center m-auto">
+          <ul className="flex flex-col laptop:flex-row gap-4">
+            {EgProduct.images.against.map((img, i) => (
+              <li className="max-w-[360px] h-auto laptop:flex-grow laptop:max-w-[360px] laptop:max-h-[700px] relative animate-fadeIn" key={i}>
+                <Image unoptimized width={360} height={360} alt={"water-products"} 
+                  src={`https://wsrv.nl/?url=${img}&default=${img}&l=9&af=''&il=''&n=-1&w=360&h=360&fit=cover&a=attention`}
+                  className="relative h-full object-fill rounded-lg border animate-fadeIn"
+                />
+              </li>
+            ))}
+          </ul>
+
+          <hr />
+
+          <ul className="flex flex-col laptop:flex-row flex-wrap">
+            {EgProduct.images.with.map((img, i) => (
+              <li className="max-w-[360px] h-auto laptop:flex-grow laptop:w-1/4 laptop:max-w-[360px] laptop:max-h-[700px] relative animate-fadeIn p-2" key={i}>
+                <Image unoptimized width={360} height={360} alt={"clean-water-products-made-in-egypt"}
+                  src={`https://wsrv.nl/?url=${img}&default=${img}&l=9&af=''&il=''&n=-1&w=360&h=360&fit=cover&a=attention`}
+                  className="relative h-full object-cover rounded-lg border animate-fadeIn"
+                />
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-      <div>
-      </div>
+
     </div>
   )
 }
+
+{/* <li className="max-w-[364px] h-auto laptop:flex-grow laptop:max-w-[364px] laptop:max-h-[700px] relative animate-fadeIn">
+              <Image unoptimized width={364} height={364} alt={""}
+                src="https://wsrv.nl/?url=https://utfs.io/f/e6461f7e-a91d-477d-a3d6-c80f2ed57ec1-zhc71c.jpg&default=https://utfs.io/f/e6461f7e-a91d-477d-a3d6-c80f2ed57ec1-zhc71c.jpg&w=364&h=364&fit=cover&a=attention"
+                className="relative h-full object-cover rounded-lg border animate-fadeIn"
+              />
+            </li> */}
+{/* <li className="max-w-[364px] h-auto laptop:flex-grow laptop:max-w-[364px] laptop:max-h-[700px] relative animate-fadeIn">
+              <Image unoptimized width={364} height={364} alt={""}
+                src="https://wsrv.nl/?url=https://utfs.io/f/e6461f7e-a91d-477d-a3d6-c80f2ed57ec1-zhc71c.jpg&default=https://utfs.io/f/e6461f7e-a91d-477d-a3d6-c80f2ed57ec1-zhc71c.jpg&w=364&h=364&fit=cover&a=entropy"
+                className="relative h-full object-cover rounded-lg border animate-fadeIn"
+              />
+            </li> */}
---
+++ data/static/products-made-in-egypt.tsx
@@ -0,0 +1,75 @@
+// import ISISHealth from '@/public/images/made-in-egypt-imgs/water/396231369_726143246215820_1088155993013934897_n.jpg'
+// import Evian from '@/public/images/made-in-egypt-imgs/water/396468867_725964209567057_1639665053392930854_n.jpg'
+// import Nestle from '@/public/images/made-in-egypt-imgs/water/396495491_725964379567040_6799430911371408126_n.jpg'
+// import Aquafina from '@/public/images/made-in-egypt-imgs/water/396517992_725964322900379_1065147077028100773_n.jpg'
+// import Dasani from '@/public/images/made-in-egypt-imgs/water/396536774_725964182900393_3566914898306823447_n.jpg'
+// import Abar from '@/public/images/made-in-egypt-imgs/water/396548385_726141906215954_953261563246650252_n.jpg'
+// import Puvana from '@/public/images/made-in-egypt-imgs/water/396549793_726141879549290_1365651147219451805_n.jpg'
+// import Baraka from '@/public/images/made-in-egypt-imgs/water/396550250_725964452900366_320547682897918169_n.jpg'
+// import AquaDelta from '@/public/images/made-in-egypt-imgs/water/396586449_726143216215823_1510718740497771467_n.jpg'
+// import Lavie from '@/public/images/made-in-egypt-imgs/water/396574418_726948359468642_3280734502114885863_n.jpg'
+// import Elano from '@/public/images/made-in-egypt-imgs/water/398791070_729661865863958_8806998312317612444_n.jpg'
+// import Siwa from '@/public/images/made-in-egypt-imgs/water/398122401_728107846019360_243850808138953914_n.jpg'
+
+export const productsMadeInEgypt = [
+  // { title: 'المياة', url: 'water', images: { against: [Evian, Nestle, Dasani, Baraka, Aquafina], with: [ISISHealth, AquaDelta, Siwa, Elano, Lavie, Abar, Puvana] } },
+  {
+    title: 'المياة', url: 'water', thumb: "https://uploadthing.com/f/83a4f401-ffaf-4347-a60e-030e21b8e8b1-cg6ue8.jpg", images: {
+      against: [
+        "https://utfs.io/f/ae485922-8d08-4d92-b4ee-637ef3c917e7-tsr1tz.jpg",
+        "https://utfs.io/f/23815423-0d84-43ba-8502-6a9f6f962cac-gdtcco.jpg",
+        "https://utfs.io/f/b5655ba3-5308-4fcc-8fc5-b3ffd6cb699d-4g3qhz.jpg",
+        "https://utfs.io/f/e5926c74-76cd-473c-90b0-b2f6de92869b-2cdvwz.jpg",
+        "https://utfs.io/f/b5772a6b-541a-4e81-a04f-4857342a2c54-d38jag.jpg"
+      ],
+      with: [
+        "https://utfs.io/f/a4bba500-f419-4757-8dd6-f28cd9428c42-f8i7yl.jpg",
+        "https://utfs.io/f/5af4d302-d2d6-4f7d-b576-b9dd79979a25-zhc71c.jpg",
+        "https://utfs.io/f/519d7b40-2370-4aca-ae22-3415ffadb8f7-mmgq5u.jpg",
+        "https://utfs.io/f/0790debe-32e4-4c95-8733-cf4f928d6b71-pcbx9m.jpg",
+        "https://utfs.io/f/f7aa213c-4052-4ed5-a2b7-2f28e4346f00-967gud.jpg",
+        "https://utfs.io/f/c283af52-0669-4828-b10a-d0da1c37734d-sm7fe6.jpg",
+        "https://utfs.io/f/3474ffa8-622c-4613-8612-7064bb1f2c35-on7l0c.jpg",
+      ]
+    }
+  },
+  // 'النسكافية',
+  // 'المنظفات',
+  // 'منتجات التجميل',
+  // 'جبن',
+  // 'صلصة',
+  // 'مربي',
+  // 'حلاوة',
+  // 'طحينة',
+  // 'نوتيلا',
+  // 'لبن مجفف',
+  // 'اللبن المعلب',
+  // 'الكورن فليكس',
+  // 'الشامبو',
+  // 'الشاور',
+  // 'مزيل العرق',
+  // 'معجون الاسنان',
+  // 'غسول الفم',
+  // 'الفوط الصحية',
+  // 'البهارات',
+  // 'مرق الدجاج',
+  // 'الصوصات',
+  // 'الأدوات المدرسية',
+  // 'الشيبسي',
+  // 'البيبسي',
+  // 'الشيكولاتة',
+  // 'الايس كريم',
+  // 'مشروبات الشعير',
+  // 'منتجات البيبي',
+  // 'مجمدات',
+  // 'سناكس',
+  // 'منتجات المطبخ',
+  // 'شركات مصرية',
+  // 'لو واقف فمحل المنظفات',
+  // 'شركات التوصيل المصرية',
+  // 'أدوات صحية مصرية',
+  // 'براندات الملابس',
+  // 'بديل بريل',
+  // 'الشوزات والملابس الرياضية',
+  // 'التونة',
+]
