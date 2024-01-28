fc8daafab44b2f78cf50d7f0115bb8316d578572
Mohamed Hakem <mohamedhakem959628@gmail.com>
2024-01-28 15:00:47 +0200

    style: only show news sub-sections - marketplace removed

--- components/navigation/secondary-related-sections-navbar.tsx
+++ components/navigation/secondary-related-sections-navbar.tsx
@@ -17,17 +17,12 @@ export default function SecondaryRelatedSections({ marketplaceSections, newsSect
     <ScrollArea dir="rtl" className="laptop:hidden w-full whitespace-nowrap justify-center items-center">
       <div className="h-fit laptop:px-4 overflow-y-auto scroll-smooth bg-[#fafafa] dark:bg-gray-800">
         <ul className="flex flex-row justify-between tablet:justify-start gap-2 font-medium no-scrollbar text-center">
-          {pathname === '/' || pathname.startsWith('/marketplace') ? marketplaceSections.map((section, i) => (
-            <Link key={i} href={`/marketplace/${section.url}`}
-              className={`w-full ${pathname === `/${section.url}` ? "text-black font-bold border-b" : "font-semibold"}`}>
-              <li className="text-black min-w-max hover:bg-gray-100 rounded-md p-2">{section.name}</li>
-            </Link>
-          )) : pathname.startsWith('/news') ? newsSections.map((section, i) => (
+          {newsSections.map((section, i) => (
             <Link key={i} href={`/news/${section.name}`}
               className={`w-full ${pathname === `/${section.name}` ? "text-black font-bold border-b" : "font-semibold"}`}>
               <li className="text-black min-w-max hover:bg-gray-100 rounded-md p-2">{section.name_ar}</li>
             </Link>
-          )) : null}
+          ))}
         </ul>
       </div>
       <ScrollBar orientation="horizontal" />
